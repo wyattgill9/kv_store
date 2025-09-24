8d0de606bcc9755e4c596992005e203a3b627621
wyattgill9 <101477459+wyattgill9@users.noreply.github.com>
2025-09-23 17:33:04 -0700

    Put 10000 items in 0.00367663 seconds. (2.71989e+06 insertions/s)

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,8 +1,13 @@
+#include <chrono>
 #include <cstddef>
+#include <functional>
 #include <future>
 #include <memory>
 #include <optional>
+#include <stop_token>
+#include <string_view>
 #include <thread>
+#include <type_traits>
 #include <unordered_map>
 #include <utility>
 #include <variant>
@@ -14,12 +19,23 @@
 #include <pthread.h>
 #endif
 
+#ifdef __APPLE__
+#include <mach/mach.h>
+#include <mach/thread_policy.h>
+#endif
+
 #include "SPSCQueue.h"
+#include "xxhash.h"
 
 template<class... Ts>
 struct overloaded : Ts... { using Ts::operator()...; };
 
-static size_t QUEUE_CAPACITY = 100;
+template<typename T, typename... Rest>
+constexpr bool multi_is_same() {
+    return (std::is_same_v<T, Rest> || ...);
+} 
+
+static size_t QUEUE_CAPACITY = 10000;
 
 template <typename K, typename V> struct Node {
 private:
@@ -34,8 +50,12 @@ private:
         std::promise<bool> reply;
     };
 
-    using RequestVariant   = std::variant<GetRequest, PutRequest>;
-    using RequestQueue     = rigtorp::SPSCQueue<RequestVariant>;
+    struct FlushRequest {
+        std::promise<void> reply;
+    };
+
+    using RequestVariant = std::variant<GetRequest, PutRequest, FlushRequest>;
+    using RequestQueue   = rigtorp::SPSCQueue<RequestVariant>;
 
     struct Shard {
         size_t                        id;
@@ -43,7 +63,8 @@ private:
         std::unique_ptr<RequestQueue> in_queue; // client requests
 
         std::jthread                  worker;
-
+        std::stop_source              stop_src;
+        
         Shard(size_t id_)
             : id(id_) {
                 in_queue = std::make_unique<RequestQueue>(QUEUE_CAPACITY);
@@ -80,17 +101,34 @@ private:
 
             return future;
         }
+
+        auto flush() -> std::future<void> {
+            FlushRequest request {
+                .reply = {}
+            };
+
+            auto future = request.reply.get_future();
+
+            while (!in_queue->try_push(std::move(request)));
+
+            return future;   
+        }
         
-        auto run() -> void {
+        auto run(std::stop_token stoken) -> void {
             pin_to_cpu(id);
 
             for(;;) {
+                if (stoken.stop_requested()) {
+                    return;
+                }
+
                 while(!in_queue->front()) {
                     std::this_thread::yield();
                 }
 
                 RequestVariant request = std::move(*in_queue->front());
                 in_queue->pop();
+
                 handle_request(std::move(request));
             }
         }
@@ -108,12 +146,18 @@ private:
                 [this](PutRequest& req) {
                     auto [it, inserted] = data.insert_or_assign(req.key, req.value);
                     req.reply.set_value(inserted);
+                },
+                // guarentees everything before it is processed
+                [this](FlushRequest& req) {
+                    req.reply.set_value();
                 }
             }, request);
         }
 
         auto start() -> void {
-            worker = std::jthread(&Shard::run, this);
+            worker = std::jthread([this](std::stop_token stoken) {
+                this->run(stoken);
+            });
         }
 
     private:
@@ -124,12 +168,27 @@ private:
             CPU_SET(cpu_id % std::thread::hardware_concurrency(), &cpuset);
             pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
 #endif
+#ifdef __APPLE__
+          thread_affinity_policy_data_t policy;
+          policy.affinity_tag = cpu_id; // 0-3 for P-cores, 4-9 for E-cores
+          thread_policy_set(mach_thread_self(), THREAD_AFFINITY_POLICY,
+                            (thread_policy_t)&policy, THREAD_AFFINITY_POLICY_COUNT);
+#endif
         }
     };
 
     size_t             id;
     size_t             num_cores;
     std::vector<Shard> shards;
+
+    // xxhash?
+    inline size_t hash_key(const K& key) {
+        if constexpr (multi_is_same<K, int, size_t>()) {
+            return key;
+        } else {
+             return std::hash<K>{}(key);
+        }
+    }
 
 public:
     explicit Node(size_t id)
@@ -143,23 +202,62 @@ public:
             shard.start();
         }
     }
-  
-    [[nodiscard]] auto insert(K key, V value) -> bool {
-        size_t shard_id = std::hash<K>{}(key) % num_cores;
-        return shards[shard_id].put(key, value).get(); 
+
+    ~Node() {
+        flush();
+        for (auto& shard : shards) {
+            shard.worker.request_stop();
+        }
+    }
+   
+    auto insert(K key, V value) -> void {
+        size_t shard_id = hash_key(key) % num_cores;
+        shards[shard_id].put(key, value);
     }
 
     auto get(K key) -> std::optional<V> {
-        size_t shard_id = std::hash<K>{}(key) % num_cores;
+        size_t shard_id = hash_key(key) % num_cores;
         return shards[shard_id].get(key).get();
+    }
+
+    auto flush() -> void {
+        std::vector<std::future<void>> futures;
+
+        for(auto& shard : shards) {
+            futures.push_back(shard.flush());
+        }
+
+        for(auto& fut : futures) {
+            fut.get();
+        }
     }
 };
 
-auto main() -> int {
-    auto node = Node<int, std::string>(0);
+template <typename K, typename V>
+Node<K, V> make_node(int id) {
+    return Node<K, V>(id);
+}
 
-    std::cout << node.insert(0, "Hello, World!") << "\n";
-    std::cout << node.get(0).value() << "\n";
+auto main() -> int {
+    using clock = std::chrono::steady_clock;
+
+    constexpr int n = 10'000;
+    auto node = make_node<size_t, std::string>(0);
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    auto start = clock::now();
+    for (size_t i = 0; i < n; ++i) {
+        node.insert(i, "value");
+    }
+    node.flush();
+    auto end = clock::now();
+
+    std::chrono::duration<double> duration = end - start;
+
+    std::cout << "Put " << n << " items in "
+              << duration.count() << " seconds. ("
+              << n * (1/duration.count()) << " insertions/s)\n";
 
     return 0;
 }
